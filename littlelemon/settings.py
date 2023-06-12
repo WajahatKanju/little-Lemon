5fd412a0707af14efbf435f2e1895f0f9b9e52ba
Wajahat Kanju <wajahat5ahmad@gmail.com>
2023-06-12 18:03:16 +0500

    bug fixes

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,19 +76,12 @@ WSGI_APPLICATION = "littlelemon.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "Littlelemon",
-        "USER": "littlelemon_web",
-        "PASSWORD": "Little@pass123",
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
-        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "Littlelemon.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
